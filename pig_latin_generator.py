edd4bd448b0b84e0e84dccdbdf6cf3367e33f180
matt-gefen <matt.gefen@gmail.com>
2020-12-27 14:48:15 -0700

    first version of pseudocode

---
+++ pig_latin_generator.py
@@ -0,0 +1,19 @@
+# To form Pig Latin, you take an English word that begins with a consonant,
+# move that consonant to the end, and then add “ay” to the end of the word.
+# If the word begins with a vowel, you simply add “way” to the end of the
+# word. 
+
+# Psuedocode
+# Introduce program
+# Take user input
+# If word begins with consonant, move the first letter to the end and add an ay
+# If word does not begin with consonant, ignore
+# If word is a joiner word (the, on, at, etc.) ignore
+# Return new phrase
+# Ask for new input or end
+
+
+import sys
+
+
+
