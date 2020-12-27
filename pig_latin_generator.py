56394f12908a35d7034dfff3def5a37e327c87eb
matt-gefen <matt.gefen@gmail.com>
2020-12-27 15:18:48 -0700

    generator working

--- pig_latin_generator.py
+++ pig_latin_generator.py
@@ -15,5 +15,40 @@
 
 import sys
 
+def pig_latin_translator(english):
+
+    while True:    
+        vowels = ('a','e','i','o','u')
+        # joiners = ('the','and','not','is','or','at','to')
+        words = english.split()
+        new_words = []
+        for word in words:
+            if word[0] in vowels:
+                new_words.append(word)
+            # elif word in joiners:
+            #     new_words.append(word)
+            else:
+                pig_letter = word[0]
+                pig_word = word[1:] + pig_letter + 'ay'
+                new_words.append(pig_word)
+        sentence = " ".join(new_words)
+        print("\n\n")
+        print(sentence.lower())
+        print("\n\n")
+        break
+
+def main():
+    
+    print('\n\nElcomeway To The Pig Latin Translator!\n')
+    while True:    
+        text = input("\n\nWrite your english sentence:\n")
+        pig_latin_translator(text)
+        try_again =  input("\n\nTry again? (Press ENTER else n to quit)\n")
+        if try_again.lower() == "n":
+            break
+
+
+if __name__ == "__main__":
+ main()
 
 
